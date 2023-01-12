a1ee5ede34ba3c51581c1d3a0b55329e6e3f602d
ruslan <rasselkom@gmail.com>
2023-01-12 12:35:46 +0300

    first commit

---
+++ src/js/components/burger.js
@@ -0,0 +1,10 @@
+// import { burger } from './functions/burger';
+
+let burger = document.querySelector('[data-burger]');
+let nav = document.querySelector('[data-nav]');
+let body = document.body;
+
+burger.addEventListener('click', () => {
+  body.classList.toggle('stop-scroll');
+  nav.classList.toggle('nav__visible');
+});
